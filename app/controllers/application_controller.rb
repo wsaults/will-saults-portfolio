eefd2317473e7fd308a1a38b5aa4e1d498e2d617
Daniel Kim and Will Saults <pair+daniel+will@childrens.com>
2017-01-09 10:10:22 -0600

    Some kinda text change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'hello, world!'
+    render html: 'All the things will be here...soon.'
   end
 end

96984c30bc94f42ab4f27e710b69e5a0bf9851f8
Daniel Kim and Will Saults <pair+daniel+will@childrens.com>
2017-01-11 12:16:49 -0600

    Added title to mountains page

--- test/controllers/mountains_controller_test.rb
+++ test/controllers/mountains_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class MountainsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get mountains_index_url
+    get mountains_url
     assert_response :success
+    assert_select "title", "...to the mountains and back"
   end
-
 end

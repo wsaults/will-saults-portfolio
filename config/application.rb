b967db7e99dc46ecad4c6fb48036d74b1ee536c7
Keenan Sturtevant and Will Saults <pair+keenan+will@childrens.com>
2017-01-19 10:14:24 -0600

    Config precompile line for heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Saults
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
   end
 end
